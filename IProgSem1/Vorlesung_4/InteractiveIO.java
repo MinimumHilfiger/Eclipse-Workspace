0281a4b5c27692acfc983ad8086b967819f1b5cf
MinimumHilfiger <philipp2.hermann@gmail.com>
2018-10-23 21:28:04 +0200

    Neuer Projektordner

---
+++ IProgSem1/Vorlesung_4/InteractiveIO.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class InteractiveIO {
+	private BufferedReader br;
+
+	public InteractiveIO() {
+		br = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+	public void write(String s) {
+		this.writeAndFlush(s);
+	}
+
+	public String promptAndRead(String s) throws Exception {
+		this.writeAndFlush(s);
+		return br.readLine();
+	}
+
+	private void writeAndFlush(String s) {
+		System.out.println(s);
+		System.out.flush();
+	}
+}
